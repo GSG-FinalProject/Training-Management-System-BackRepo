25187f5ea6c7e02b43eb704e478247943cedd7ee
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-10-03 15:47:24 +0300

    add CORS and Swagger Configuration

---
+++ TMS.Api/TMS.Api/Extensions/ModulePresentationDependencies.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using TMS.Api.Logger;
+
+namespace TMS.Api.Extensions;
+public static class ModulePresentationDependencies
+{
+    public static IServiceCollection AddPresentationDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IConfiguration>(configuration);
+
+        services.AddIdentity<User, IdentityRole>()
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+        services.AddScoped<IResponseHandler, ResponseHandler>();
+
+
+        services.AddScoped<IUserManager, UserManager>();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+                .AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration["JWT:Issuer"],
+                        ValidAudience = configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
+                    };
+                });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+        });
+
+        services.AddControllers(options =>
+        {
+            options.CacheProfiles.Add("DefaultCache", new CacheProfile
+            {
+                Duration = 30,
+                Location = ResponseCacheLocation.Any
+            });
+        });
+
+        services.AddAutoMapper(typeof(Program).Assembly);
+
+        services.AddScoped<ILog, Log>();
+        services.AddScoped<ITrainerRepository, TrainerRepository>();
+        services.AddScoped<IResponseHandler, ResponseHandler>();
+        services.AddScoped<ITraineeRepository, TraineeRepository>();
+
+        return services;
+    }
+}

03525419730e8bad56649157b5dbb3cb31348f25
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-10-03 16:13:21 +0300

    create DTOs class and Abstracts repository

---
+++ TMS.Domain/DTOs/Admin/AdminResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using TMS.Domain.Enums;
+namespace TMS.Domain.DTOs.Admin;
+public class AdminResponseDto
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public Role UserType { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ TMS.Domain/DTOs/Trainee/RegisterTraineeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TMS.Domain.DTOs.Trainee;
+public class RegisterTraineeDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public String TrainingProgram { get; set; }
+    public int TrainingHours { get; set; }
+}

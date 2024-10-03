f582a17dbb3db322819fa8f0aad26956fa573ccf
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-10-03 16:16:40 +0300

    Implementation generic repository

---
+++ TMS.Infrastructure/DbContexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TMS.Domain.Entities;
+namespace TMS.Infrastructure.DbContexts;
+public class AppDbContext : IdentityDbContext<User>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Trainer> Trainers { get; set; }
+    public DbSet<Trainee> Trainees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Admin>().ToTable("Admins");
+        modelBuilder.Entity<Trainer>().ToTable("Trainers");
+        modelBuilder.Entity<Trainee>().ToTable("Trainees");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ TMS.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TMS.Domain.Interfaces.Persistence.Repositories;
+using TMS.Infrastructure.DbContexts;
+namespace TMS.Infrastructure.Repositories;
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly AppDbContext _appDbContext;
+    public GenericRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        await _appDbContext.Set<T>().AddAsync(entity);
+        await _appDbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<string> DeleteAsync(string id)
+    {
+        var entity = await _appDbContext.Set<T>().FindAsync(id);
+
+        if (entity is null)
+            throw new KeyNotFoundException($"Entity with ID {id} not found.");
+
+        _appDbContext.Set<T>().Remove(entity);
+        await _appDbContext.SaveChangesAsync();
+
+        return "Entity deleted successfully.";
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _appDbContext.Set<T>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(string id)
+    {
+        var entity = await _appDbContext.Set<T>().FindAsync(id);
+
+        if (entity is null)
+            throw new KeyNotFoundException($"Entity with ID {id} not found.");
+
+        return entity;
+    }
+
+    public async Task<T> UpdateAsync(string id, T entity)
+    {
+        var existingEntity = await _appDbContext.Set<T>().FindAsync(id);
+
+        if (existingEntity is null)
+            throw new KeyNotFoundException($"Entity with ID {id} not found.");
+
+        _appDbContext.Entry(existingEntity).CurrentValues.SetValues(entity);
+        await _appDbContext.SaveChangesAsync();
+
+        return existingEntity;
+    }
+}
